0e422769b6498556755db32ab03dc855be214d47
fbarzin <fbarzin84@gmail.com>
2025-01-31 16:44:59 -0500

    Converts all the <p> tags to <div> since they might cause an issue when there are other tags nested in the <p> tag.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
     <html lang="en" className="dark scroll-smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        suppressHydrationWarning
       >
         <NavBar />
         <div className="container mx-auto max-w-screen-2xl px-0 md:px-4">
