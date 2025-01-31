793ca9978d63283848bef81d8f9a05f2a3bda3ab
fbarzin <fbarzin84@gmail.com>
2025-01-31 10:53:03 -0500

    Updates the skills section and improves the badges.

--- src/components/SkillBadge.tsx
+++ src/components/SkillBadge.tsx
@@ -1,3 +1,4 @@
+import { getSkillBadge } from '@/lib/skill-badge.utils';
 import React from 'react';
 
 export type SkillBadge = {
@@ -7,40 +8,56 @@ export type SkillBadge = {
 };
 
 function SkillBadge({ skillBadge }: { skillBadge: SkillBadge }) {
+  const skillBadgeData = getSkillBadge(skillBadge.skill);
+  if (!skillBadgeData) {
+    return <></>;
+  }
+
+  const skillBadgeColor = skillBadgeData.color;
+  const foregroundColor = getContrastColor(skillBadgeColor);
+
   return (
     <a href={skillBadge.website} target="_blank" rel="noreferrer">
-      <div className="flex flex-row items-center rounded-lg border border-sky-900">
-        <div className="rounded-l-md bg-sky-900 px-2 py-1 text-sm font-semibold">
-          {skillBadge.skill}
-        </div>
-        <div className="rounded-r-md bg-sky-300 px-2 py-1 text-sm font-semibold text-black">
-          {convertSkillLevelToString(skillBadge.level)}
-        </div>
+      <div
+        className={`text-md flex flex-row items-center rounded-sm border-2 border-solid border-gray-700 px-2 py-1 font-semibold`}
+        style={{
+          backgroundColor: skillBadgeColor,
+          color: foregroundColor,
+        }}
+      >
+        <div
+          className="mr-2 h-4 w-4"
+          dangerouslySetInnerHTML={{ __html: skillBadgeData.logo }}
+          style={{ fill: foregroundColor }}
+        />
+        {skillBadgeData.label}
       </div>
     </a>
   );
 }
 
-/**
- * Converts the skill level to a string
- * @param level 1-5
- * @returns
- */
-function convertSkillLevelToString(level: number): string {
-  switch (level) {
-    case 1:
-      return 'Just Started';
-    case 2:
-      return 'Beginner';
-    case 3:
-      return 'Intermediate';
-    case 4:
-      return 'Advanced';
-    case 5:
-      return 'Expert';
-    default:
-      return 'Unknown';
+function getContrastColor(hex: string): string {
+  // Convert hex to RGB
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  // Calculate luminance
+  const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+
+  // Calculate contrast color
+  const contrast = luminance > 0.5 ? '#000000' : '#FFFFFF';
+
+  // Specific case for colors close to React's color
+  if (
+    Math.abs(r - 97) < 10 &&
+    Math.abs(g - 218) < 10 &&
+    Math.abs(b - 251) < 10
+  ) {
+    return '#000000';
   }
+
+  return contrast;
 }
 
 export default SkillBadge;
